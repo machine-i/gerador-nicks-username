0d67ac9623f3521bfc57b263290fff2331f34e6d
Wanderson Rene <wanderson.rene12@gmail.com>
2022-08-06 15:26:08 -0300

    commit inicial

---
+++ gerador.py
@@ -0,0 +1,26 @@
+from faker import Faker
+import PySimpleGUI as psg
+import clipboard as c
+
+psg.theme('DarkBlue1')
+
+lyt = [
+    [psg.Text('Clique em gerar', size=(30, 1))],
+    [psg.Button('gerar'), psg.Button('copiar')],
+    [psg.Text(key='nicks')]
+]
+
+wind = psg.Window('Gerador de nicks', lyt, finalize=True)
+fk = Faker()
+nick = ''
+
+while True:
+    e, v = wind.read()
+    if e == psg.WIN_CLOSED:
+        break
+    if e == 'gerar':
+        nick = fk.simple_profile()['username']
+        wind['nicks'].update(nick)
+    if e == 'copiar':
+        c.copy(nick)
+wind.close()
